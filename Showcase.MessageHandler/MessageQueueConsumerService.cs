b0ad90a4c3dd22831bd9681d0997e4ab9363972a
Claudio Schaad <schaad@pog.ch>
2022-07-31 21:45:44 +0200

    Change message handler to hosted app

---
+++ Showcase.MessageHandler/MessageQueueConsumerService.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using RabbitMQ.Client;
+using Showcase.Infrastructure.Messaging;
+using Showcase.Infrastructure.Messaging.RabbitMQ;
+using Showcase.Measurement.Application;
+using Showcase.Measurement.Domain.Finance;
+using Showcase.Measurement.Domain.Weather;
+
+namespace Showcase.MessageHandler
+{
+    internal sealed class MessageQueueConsumerService : IHostedService
+    {
+        private readonly ILogger logger;
+        private readonly IHostApplicationLifetime appLifetime;
+        private readonly IOptions<MessagingOptions> messagingOptions;
+        private readonly IWeatherMeasurementService weatherMeasurementService;
+        private readonly IFinanceMeasurementService financeMeasurementService;
+
+        public MessageQueueConsumerService(
+            ILogger<MessageQueueConsumerService> logger, 
+            IHostApplicationLifetime appLifetime, 
+            IOptions<MessagingOptions> messagingOptions, 
+            IWeatherMeasurementService weatherMeasurementService, 
+            IFinanceMeasurementService financeMeasurementService)
+        {
+            this.logger = logger;
+            this.appLifetime = appLifetime;
+            this.messagingOptions = messagingOptions;
+            this.weatherMeasurementService = weatherMeasurementService;
+            this.financeMeasurementService = financeMeasurementService;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            logger.LogDebug($"Starting with arguments: {string.Join(" ", Environment.GetCommandLineArgs())}");
+
+            appLifetime.ApplicationStarted.Register(() =>
+            {
+                Task.Run(async () =>
+                {
+                    try
+                    {
+                        logger.LogInformation("Starting Message Handler (please ensure rabbitmq is up)...");
+
+                        var factory = new ConnectionFactory() { HostName = messagingOptions.Value.Hostname, Password = messagingOptions.Value.Password, UserName = messagingOptions.Value.Username, Port = messagingOptions.Value.Port };
+                        using (var connection = factory.CreateConnection())
+                        using (var channel = connection.CreateModel())
+                        {
+                            RabbitMqHelper.Init(channel, messagingOptions.Value.Queue);
+
+                            RabbitMqHelper.Consume<WeatherRecord>(channel, messagingOptions.Value.Queue, async (T) =>
+                            {
+                                logger.LogInformation($"{T}");
+                                await weatherMeasurementService.SaveWeatherAsync(T, CancellationToken.None);
+                            });
+
+                            RabbitMqHelper.Consume<StockRecord>(channel, messagingOptions.Value.Queue, async (T) =>
+                            {
+                                logger.LogInformation($"{T}");
+                                await financeMeasurementService.SaveStockAsync(T, CancellationToken.None);
+                            });
+
+                            while (!cancellationToken.IsCancellationRequested)
+                            {
+                                await Task.Delay(1000);
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.LogError(ex, "Unhandled exception!");
+                    }
+                    finally
+                    {
+                        appLifetime.StopApplication();
+                    }
+                });
+            });
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
--- Showcase.MessageHandler/Program.cs
+++ Showcase.MessageHandler/Program.cs
@@ -1,69 +1,32 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using RabbitMQ.Client;
 using Showcase.Infrastructure;
 using Showcase.Infrastructure.Messaging;
-using Showcase.Infrastructure.Messaging.RabbitMQ;
-using Showcase.Measurement.Application;
-using Showcase.Measurement.Domain.Finance;
-using Showcase.Measurement.Domain.Weather;
+using System.Reflection;
 
 namespace Showcase.MessageHandler
 {
+    // https://dfederm.com/building-a-console-app-with-.net-generic-host/
     internal class Program
     {
-        static void Main(string[] args)
+        private static async Task Main(string[] args)
         {
-            Console.WriteLine("Starting Message Handler (please ensure rabbitmq is up)...");
+            await Host.CreateDefaultBuilder(args)
+                .UseContentRoot(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location))
+                .ConfigureLogging(logging =>
+                {
+                    // Add any 3rd party loggers like NLog or Serilog
+                })
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services
+                        .AddHostedService<MessageQueueConsumerService>()
+                        .AddInfrastructureServices(hostContext.Configuration);
 
-            var configuration = CreateConfiguration(args);
-            using var host = CreateHost(args, configuration);
-            using var serviceScope = CreateServiceScope(host);
-
-            var options = configuration.GetSection(MessagingOptions.SectionKey).Get<MessagingOptions>();
-            var factory = new ConnectionFactory() { HostName = options.Hostname, Password = options.Password, UserName = options.Username, Port = options.Port };
-            using (var connection = factory.CreateConnection())
-            using (var channel = connection.CreateModel())
-            {
-                RabbitMqHelper.Init(channel, options.Queue);
-
-                var weatherMeasurementService = serviceScope.ServiceProvider.GetRequiredService<IWeatherMeasurementService>();
-                RabbitMqHelper.Consume<WeatherRecord>(channel, options.Queue, async (T) => await weatherMeasurementService.SaveWeatherAsync(T, CancellationToken.None));
-
-                var financeMeasurementService = serviceScope.ServiceProvider.GetRequiredService<IFinanceMeasurementService>();
-                RabbitMqHelper.Consume<StockRecord>(channel, options.Queue, async (T) => await financeMeasurementService.SaveStockAsync(T, CancellationToken.None));
-
-                Console.WriteLine(" Press [enter] to exit.");
-                Console.ReadLine();
-            } 
-        }
-
-        private static IConfiguration CreateConfiguration(string[] args)
-        {
-            var configuration = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddEnvironmentVariables()
-                .AddCommandLine(args)
-                .Build();
-
-            return configuration;
-        }
-
-        private static IHost CreateHost(string[] args, IConfiguration configuration)
-        {
-            var host = Host.CreateDefaultBuilder(args)
-                .ConfigureServices((_, services) =>
-                    services.AddInfrastructureServices(configuration))
-                .Build();
-
-            return host;
-        }
-
-        private static IServiceScope CreateServiceScope(IHost host)
-        {
-            var serviceScope = host.Services.CreateScope();
-            return serviceScope;
+                    services.AddOptions<MessagingOptions>().Bind(hostContext.Configuration.GetSection(MessagingOptions.SectionKey));
+                })
+                .RunConsoleAsync();
         }
     }
 }
