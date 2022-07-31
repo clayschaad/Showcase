120c35826a3f194c5faefab0999bf5963c207143
Claudio Schaad <schaad@pog.ch>
2022-07-31 22:06:54 +0200

    Add default values for api

---
+++ Showcase.WebApi/Controllers/FinanceMeasurementController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Showcase.Measurement.Application;
+
+namespace Showcase.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FinanceMeasurementController : ControllerBase
+    {
+        private readonly IFinanceMeasurementService financeMeasurementService;
+
+        public FinanceMeasurementController(IFinanceMeasurementService financeMeasurementService)
+        {
+            this.financeMeasurementService = financeMeasurementService;
+        }
+
+        [HttpPost(Name = "GetStock")]
+        public async Task PostAsync(CancellationToken cancellation, DateTime date, string symbol = "UBS")
+        {
+            if (date == DateTime.MinValue) 
+            {
+                date = DateTime.Now.AddDays(-1);
+            }
+
+            await financeMeasurementService.MeasureStockAsync(symbol, date, cancellation);
+        }
+    }
+}
---
+++ Showcase.WebApi/Controllers/WeatherMeasurementController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Showcase.Measurement.Application;
+
+namespace Showcase.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherMeasurementController : ControllerBase
+    {
+        private readonly IWeatherMeasurementService weatherMeasurementService;
+
+        public WeatherMeasurementController(IWeatherMeasurementService weatherMeasurementService)
+        {
+            this.weatherMeasurementService = weatherMeasurementService;
+        }
+
+        [HttpPost(Name = "GetWeather")]
+        public async Task PostAsync(CancellationToken cancellationToken, double latitude = 47.56565386661019, double longitude = 9.108359461592157)
+        {
+            await weatherMeasurementService.MeasureWeatherAsync(latitude: latitude, longitude: longitude, cancellationToken);
+        }
+    }
+}
